affc86332a3b089578d1c21c7f483b183499ec1d
Mike Lewis <ft.mikelewis@gmail.com>
2011-04-30 02:20:07 -0400

    Updated gitignore

--- examples/workers/basic.rb
+++
@@ -1,9 +0,0 @@
-#testing
-f = File.symlink?(__FILE__) ? File.readlink(__FILE__) : __FILE__
-require File.join(File.dirname(f), '..', '..', 'lib', 'napalm')
-
-class MyWorker < Napalm::Worker
-
-end
-
-w = MyWorker.do_work
